d1091015b3f7903ca5b6837acf9ea775556522e1
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-17 15:49:25 +0100

    VideoController implemented

--- videoservice/src/main/java/com/codecool/videoservice/controller/VideoContorller.java
+++ videoservice/src/main/java/com/codecool/videoservice/controller/VideoContorller.java
@@ -1,4 +1,23 @@
 package com.codecool.videoservice.controller;
 
+import com.codecool.videoservice.modell.Video;
+import com.codecool.videoservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/video")
 public class VideoContorller {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @GetMapping
+    public List<Video> allVideos() {
+        return videoRepository.findAll();
+    }
 }
