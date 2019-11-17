68365832ece50d59ae651d3f571ca0efd4a2955f
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-17 16:00:23 +0100

    JpaRepository added

--- videoservice/src/main/java/com/codecool/videoservice/repository/VideoRepository.java
+++ videoservice/src/main/java/com/codecool/videoservice/repository/VideoRepository.java
@@ -1,6 +1,8 @@
 package com.codecool.videoservice.repository;
 
 import com.codecool.videoservice.modell.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
 }
