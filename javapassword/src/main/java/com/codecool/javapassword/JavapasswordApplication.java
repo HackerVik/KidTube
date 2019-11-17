84b7d39186d69435329c9fc4a534ee2f753dc51d
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-17 23:12:27 +0100

    java password generator created

---
+++ javapassword/src/main/java/com/codecool/javapassword/JavapasswordApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.javapassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapasswordApplication.class, args);
+	}
+
+}
