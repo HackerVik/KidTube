017ac19f7b67afb6be05cb29f2adc5bb7f27c376
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-17 15:56:05 +0100

    Video class implemented

--- videoservice/src/main/java/com/codecool/videoservice/modell/Video.java
+++ videoservice/src/main/java/com/codecool/videoservice/modell/Video.java
@@ -1,4 +1,31 @@
 package com.codecool.videoservice.modell;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Video {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String url;
+
 }
