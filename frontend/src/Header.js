0b841d0186d74c39a96db1653dd350e05048d096
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-23 23:57:34 +0100

    Header component upgraded

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -7,7 +7,6 @@ function Header() {
             <ul>
                 <li><img src={'./static/img/kidlogo.png'} alt={'KidTube logo'}/></li>
                 <li><h1>KidTube</h1></li>
-                <li><input type="text" placeholder="Search.."/></li>
             </ul>
         </div>
     );
