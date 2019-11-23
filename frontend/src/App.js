05e5965f03a54264aaf924a29d99b53c450dade5
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-23 23:52:41 +0100

    reorganized App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from "./Header";
 import ActualVideo from "./ActualVideo";
 import VideoList from "./VideoList";
+import Navbar from "./Navbar";
 
 function App() {
-    return(
-        <div>
-          <Header/>
-          <ActualVideo/>
-          <VideoList/>
+    return (
+        <div className="app">
+            <header className="app-header">
+                <Header/>
+                <Navbar/>
+            </header>
+            <body className="app-body">
+            <ActualVideo/>
+            <VideoList/>
+            </body>
         </div>
     );
 }
