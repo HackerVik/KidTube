0c6517c7ff86fd6eed82499290cb845e92214b0a
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-25 22:19:59 +0100

    Videocontext created

---
+++ frontend/src/context/VideoContext.js
@@ -0,0 +1,29 @@
+import React, {createContext, useEffect, useReducer} from 'react';
+import axios from "axios";
+import videoreducer from "../reducer/VideoReducer";
+
+export const videocontext = createContext();
+
+export default function VideoContext(props) {
+    const initialState = [{
+        id: 0,
+        name: '',
+        url: '',
+        ispublic: false,
+        owner: ''
+    }];
+    const [videos, dispatch] = useReducer(videoreducer, initialState);
+    const fetchdata = () => {
+        axios.get('http://' + process.env.REACT_APP_CLIENT_IP + ':8081/video/all').then(response => {
+            const data = response.data;
+            dispatch({type: 'initialdata', data})
+        });
+    };
+    useEffect(() => {
+        fetchdata()
+    },[]);
+    return (
+        <videocontext.Provider value={{videos, dispatch}}>
+            {props.children}</videocontext.Provider>
+    );
+}
