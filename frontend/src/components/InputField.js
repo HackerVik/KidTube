e47cf42c8e16e33832728735da254cd86c2605a7
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-26 17:39:18 +0100

    added TextField components

---
+++ frontend/src/components/InputField.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+}));
+
+export default function InputTextField(props) {
+    const classes = useStyles();
+    const {label, helpertext} = props;
+    return (
+        <div className={classes.container}>
+            <div>
+                <TextField
+                    label={label}
+                    id="outlined-margin-normal"
+                    className={classes.textField}
+                    helperText={helpertext}
+                    margin="normal"
+                    variant="outlined"
+                />
+            </div>
+        </div>
+    );
+}
