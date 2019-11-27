d10756637831f642bc767fc13c8635d01b17d630
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-27 16:42:50 +0100

    JPA qury for search

--- videoservice/src/main/java/com/codecool/videoservice/repository/VideoRepository.java
+++ videoservice/src/main/java/com/codecool/videoservice/repository/VideoRepository.java
@@ -3,6 +3,9 @@ package com.codecool.videoservice.repository;
 import com.codecool.videoservice.modell.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    List<Video> findAllByNameContainsIgnoreCase (String search);
 }
