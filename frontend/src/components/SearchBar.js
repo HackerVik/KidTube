f3022188eebac1f53cd8ad86d21b9912fcd82b82
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-27 21:56:08 +0100

    grid with css

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,20 +1,9 @@
 import React, {useContext, useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import {videocontext} from "../context/VideoContext";
 import {InputBase} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: 130,
-        },
-    },
-}));
-
 export default function SearchBar() {
-    const classes = useStyles();
     const [searchString, setSearch] = useState("");
     const {search, fetchdata} = useContext(videocontext);
     const searchClick = () => {
@@ -23,10 +12,16 @@ export default function SearchBar() {
 
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <SearchIcon onClick={searchClick}/>
-            <InputBase placeholder="Search" onChange={(e) => setSearch(e.target.value)}
-                       inputProps={{'aria-label': 'search'}}/>
-        </form>
+        <div className="searchbar-grid-container">
+            <form>
+                <div className="searchbar-grid-item">
+                    <InputBase placeholder="Search" onChange={(e) => setSearch(e.target.value)}
+                               inputProps={{'aria-label': 'search'}}/>
+                </div>
+                <div className="searchbar-grid-item">
+                    <SearchIcon onClick={searchClick}/>
+                </div>
+            </form>
+        </div>
     );
 }
