8498d481b39d95888fe29bbff3f60e3f5ab8a6cf
BihackerViktor <bihacker.viktor@gmail.com>
2019-11-27 16:46:25 +0100

    header redo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,8 +4,6 @@ import '../App.css';
 function Header() {
     return (
         <div className="header">
-                <img src={'./static/img/kidlogo.png'} alt={'KidTube logo'}/>
-                <h1>KidTube</h1>
         </div>
     );
 }
