93ced792322e6d0dc1d401aec6b6384e31fe06d8
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-05 15:55:19 +0100

    css moved to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import VideoList from "./components/VideoList";
 import VideoContextProvider from "./context/VideoContext";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import {makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     videocontainer: {
         flexGrow: 1,
     },
+    app: {
+      textAlign: 'center',
+    },
 }));
 
 function App() {
     const classes = useStyles();
     return (
-        <div className="app">
+        <div className={classes.app}>
             <Header/>
             <VideoContextProvider>
                 <Navbar/>
