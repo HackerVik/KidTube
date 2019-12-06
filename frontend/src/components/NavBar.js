b0a463fb676ece44f75f86ab90cb660de3b40d48
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-06 15:58:47 +0100

    unused variable removed

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {fade, makeStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -15,85 +15,27 @@ import Search from "./Search";
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/core/SvgIcon/SvgIcon";
 import Typography from "@material-ui/core/Typography";
 import DehazeIcon from '@material-ui/icons/Dehaze';
 
 const useStyles = makeStyles(theme => ({
-    grow: {
-        flexGrow: 1,
-    },
     navbar: {
         color: 'grey',
         background: '#fff',
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
     title: {
-        display: 'none',
+        minWidth: '5em',
         color: 'red',
-        fontWeight: 'bold',
-        fontSize: '1.8em',
         textShadow: '.1em .1em .1em grey',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        },
-    },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
-    },
-    sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
+        fontWeight: 'bold',
     },
     paper: {
         backgroundColor: 'white',
-        width: '25em',
+        width: 'auto',
+        padding: '1em',
     },
     modal: {
         display: 'flex',
-        padding: theme.spacing(1),
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -174,15 +116,11 @@ export default function NavBar() {
             <div className={classes.grow}>
                 <AppBar position="static" className={classes.navbar}>
                     <Toolbar>
-                        <IconButton
-                            edge="start"
-                            className={classes.menuButton}
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={toggleDrawer('right', true)}
-                        >
-                            <DehazeIcon/></IconButton><MenuIcon/>
-                        <Typography className={classes.title} variant="h6" noWrap>KidTube</Typography>
+                        <IconButton onClick={toggleDrawer('right', true)} edge="start" color="inherit"
+                                    aria-label="open drawer">
+                            <DehazeIcon/>
+                        </IconButton>
+                        <Typography className={classes.title} variant="h5" noWrap>KidTube</Typography>
                         <SortSelect/>
                         <Search/>
                     </Toolbar>
