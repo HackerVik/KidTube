cccfb410ae9cca42c55fa43c9efe79c3db79fc3c
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-06 10:39:36 +0100

    customized icons

--- frontend/src/components/VideoRating.js
+++ frontend/src/components/VideoRating.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Rating from "@material-ui/lab/Rating";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import {withStyles} from '@material-ui/core/styles';
+
+
+const StyledRating = withStyles({
+    iconFilled: {
+        color: '#ff6d75',
+    },
+    iconHover: {
+        color: '#ff3d47',
+    },
+})(Rating);
+
+function getLabelText(value) {
+    return `${value} Heart${value !== 1 ? 's' : ''}`;
+}
 
 export default function VideoRating(props) {
-    const [value, setValue] = React.useState(2);
     return (
         <div>
-            <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
+            <StyledRating
+                name="customized-color"
+                value={2}
+                getLabelText={getLabelText}
+                icon={<FavoriteIcon fontSize="inherit"/>}
             />
         </div>
     );
