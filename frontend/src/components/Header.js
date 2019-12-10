9651d9c7e3f5eb166ae1244a28b607cb15e63e42
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-10 11:17:14 +0100

    blur on load

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {makeStyles} from "@material-ui/core";
 import background from '../background.png';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        width: 'auto',
-        height: '8em',
-        padding: '1em',
-        opacity: '.65',
-        backgroundImage: `url(${background})`,
-    },
-}));
+import {videocontext} from "../context/VideoContext";
 
 export default function Header() {
+    const {isLoading} = useContext(videocontext);
+    const useStyles = makeStyles(theme => ({
+        root: {
+            width: 'auto',
+            height: '7em',
+            padding: '1em',
+            opacity: '.65',
+            backgroundImage: `url(${background})`,
+            filter: isLoading ? 'blur(5px)' : 'blur(0px)',
+        },
+    }));
     const classes = useStyles();
     return (
         <div className={classes.root}/>
