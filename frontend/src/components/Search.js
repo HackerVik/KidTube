fa16802d6a1714b893b366cc65180da56b9e9057
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-10 11:18:55 +0100

    radius fix

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -11,20 +11,23 @@ const useStyles = makeStyles(theme => ({
         color: 'grey',
         backgroundColor: '#fff',
         border: '1px solid lightgrey',
-        borderRadius: '.3em',
+        borderRadius: '1em',
         display: 'flex',
         padding: '.65em',
         width: '9em',
+        transition: 'width .3s ease-out 0s',
         '&:hover': {
             border: '2px solid lightblue',
-        }
+            width: '15em',
+        },
     },
     hover: {
         '&:hover': {
             background: 'lightblue',
             borderRadius: '1em',
         },
-      },
+    },
+
 }));
 
 export default function Search() {
@@ -37,7 +40,8 @@ export default function Search() {
     return (
         <div className={classes.root}>
             <form>
-                <InputBase className={classes.hover} placeholder="Search... " onChange={(e) => setSearch(e.target.value)}
+                <InputBase className={classes.hover} placeholder="Search... "
+                           onChange={(e) => setSearch(e.target.value)}
                            inputProps={{'aria-label': 'search'}}/>
             </form>
             <SearchIcon className={classes.hover} onClick={searchClick}/>
