3474476ff1d15cd3b9cfbdb76727f9a42ad23ef3
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-11 17:49:30 +0100

    passwordmeter created

---
+++ frontend/src/components/PasswordMeter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './PasswordStrengthMeter.css';
+
+export default class PasswordStrengthMeter extends Component {
+    render() {
+        return (
+            <div className="password-strength-meter">
+                I'm a password strength meter
+            </div>
+        );
+    }
+}
