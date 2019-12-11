ab9e966bc7eb876ba76778c2f506d78828e9aa2a
BihackerViktor <bihacker.viktor@gmail.com>
2019-12-11 16:25:35 +0100

    added marginTop

--- frontend/src/components/VideoList.js
+++ frontend/src/components/VideoList.js
@@ -1,12 +1,20 @@
 import React, {useContext} from 'react';
 import Video from "./Video";
 import {videocontext} from "../context/VideoContext";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        marginTop: '1em',
+    },
+},);
 
 export default function VideoList() {
+    const classes = useStyles();
     const {videos, isLoading} = useContext(videocontext);
     return (
         !isLoading ?
-            <div>
+            <div className={classes.root}>
                 {videos.map(video => {
                     return (<Video video={video} key={video.id}/>)
                 })}
